1ddd3c3f1c2c468017816bb9821e6783f0740a97
themikaelson <34260590+themikaelson@users.noreply.github.com>
2021-03-02 00:50:51 -0500

    update for production

--- server.js
+++ server.js
@@ -21,6 +21,15 @@ app.use("/api/users", require("./routes/users"));
 app.use("/api/auth", require("./routes/auth"));
 app.use("/api/notes", require("./routes/notes"));
 
+// Serve Static assest in production
+if (process.env.NODE_ENV === "production") {
+	// Set Static folder
+	app.use(express.static("client/build"));
+	app.get("*", (req, res) => {
+		res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+	});
+}
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
 	console.log(`Server is running at port ${PORT}`);
